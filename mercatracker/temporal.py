38a9cbe77bc4fed79079d3821e31a630d201b7cc
Miguel Claramunt <26309689+MiguelClaramunt@users.noreply.github.com>
2024-05-14 14:11:05 +0200

    Refactoring of temporal functions

---
+++ mercatracker/temporal.py
@@ -0,0 +1,21 @@
+import datetime
+
+from mercatracker import globals
+
+config = globals.load_dotenv()
+
+
+def iso_date2custom_format(iso_date: str, custom_format: str) -> str:
+    date = datetime.date.fromisoformat(iso_date)
+    return date.strftime(custom_format)
+
+
+def updated_ids() -> bool:
+    """Checks if ids in `.env.temp` file are updated, avoiding unnecesary requests to API.
+
+    Returns:
+        bool: True if ids in `.env.temp` are updated
+    """
+    return iso_date2custom_format(
+        str(datetime.date.today()), custom_format=config["FORMAT_DATE"]
+    ) == str(config["LASTMOD_DATE"])
