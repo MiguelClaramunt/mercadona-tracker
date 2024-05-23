a3151c439f18ec313063f898865acae428caed4d
Miguel Claramunt <claramuntmiguel@gmail.com>
2024-05-23 17:21:39 +0200

    CLN/DOC: Refactored `globals` into `config`, added documentation. ENH: Added slicing for `Config` class and `update` method.

--- src/mercatracker/config.py
+++ src/mercatracker/config.py
@@ -1,30 +1,38 @@
 import ast
 import glob
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from itertools import chain
-from typing import Dict, List, Tuple, Union
+from pathlib import Path
+from typing import Dict, Iterable, List, Union
 
 import dotenv
 
 
 @dataclass
 class Config:
-    paths: Union[List[str,], Tuple[str,], None] = None
+    paths: Union[Dict[str, str], None] = None
     os: Union[bool, None] = None
+    dict_: dict = field(init=False)
 
-    def __getitem__(self, item):
-        return self.dict_.__getitem__(item)
+    def __getitem__(self, item: Union[str, Iterable[str,]]):
+        if isinstance(item, str):
+            return self.dict_[item]
+        elif isinstance(item, Iterable) and all(isinstance(i, str) for i in item):
+            return *(self.dict_[key] for key in item),
+        else:
+            raise TypeError("Key must be a string or a list of strings.")
 
     @classmethod
     def _search(cls, path: str) -> list:
-        return glob.glob(pathname=path)
+        return {Path(path).stem: path for path in glob.glob(pathname=path)}
 
     @classmethod
     def _load_dotenv(cls, paths: List[str,]) -> Dict[str, str | None]:
         return dict(
             chain.from_iterable(
-                dotenv.dotenv_values(dotenv_path=path).items() for path in paths
+                dotenv.dotenv_values(dotenv_path=path).items()
+                for path in paths.values()
             )
         )
 
@@ -44,7 +52,7 @@ class Config:
 
         return self
 
-    def load(self, postprocess: bool = True) -> dict:
+    def load(self, postprocess: bool = True) -> "Config":
         self.dict_ = self._load_dotenv(self.paths)
         self._refresh()
 
@@ -53,21 +61,24 @@ class Config:
                 meta_var="COLS_WITH_ALIASES"
             )._generate_etl_parameters(meta_var="ETL_PARAMETERS")
 
-        return self.dict_
+        return self
 
     def _convert_aliases(self, meta_var: str = "COLS_WITH_ALIASES") -> "Config":
         """Convert aliases into its values from a meta-variable.
 
-        From this aliases `YMD`, `CATEGORIES_FINAL`; and the meta-variable `COLS_WITH_ALIASES`:
-        * `YMD = ["ymd"]`
-        * `CATEGORIES_FINAL = ["level", "name"]`
-        * `COLS_WITH_ALIASES = [..., "_CATEGORIES_COLS", ...]`
+        From aliases `YMD`, `CATEGORIES_FINAL`; and the meta-variable
+        `COLS_WITH_ALIASES` contained in `shared.env` file:
+        - `YMD = ["ymd"]`
+        - `CATEGORIES_FINAL = ["level", "name"]`
+        - `COLS_WITH_ALIASES = [..., "_CATEGORIES_COLS", ...]`
 
-        We process the aliases in `_CATEGORIES_COLS`, resulting in the final list of variables:
-        * `_CATEGORIES_COLS = ["CATEGORIES_FINAL", "YMD"] -> ["level", "name", "ymd"]`
+        We process the aliases in `_CATEGORIES_COLS`, resulting in the final
+        list of variables:
+        - `_CATEGORIES_COLS = ["CATEGORIES_FINAL", "YMD"] -> ["level", "name", "ymd"]`
 
         Args:
-            meta_var (str, optional): Meta-variable, contains all variables to convert. Defaults to "COLS_WITH_ALIASES".
+            meta_var (str, optional): Meta-variable, contains all variables
+            to convert. Defaults to `"COLS_WITH_ALIASES"`.
         """
         for var in self.dict_[meta_var]:
             self.dict_[var] = sum(
@@ -83,3 +94,9 @@ class Config:
                     self.dict_[meta_var][i][column] = self.dict_[parameters[column]]
                 except TypeError:
                     continue
+
+        return self
+
+    def update(self, var: Dict[str, str]) -> None:
+        for k, v in var.items():
+            dotenv.set_key(dotenv_path=self.paths["temp"], key_to_set=k, value_to_set=v)
