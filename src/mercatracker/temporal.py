47f68a40a50de5efcb9fa9f51c33bd2d558dd8b4
Miguel Claramunt <claramuntmiguel@gmail.com>
2024-10-08 13:26:39 +0200

    20241008

--- src/mercatracker/temporal.py
+++ src/mercatracker/temporal.py
@@ -5,7 +5,7 @@ from mercatracker.config import Config
 config = Config().load()
 
 
-def iso_date2custom_format(iso_date: str, custom_format: str) -> str:
+def iso_date2custom_format(iso_date: str, custom_format: str = r"%Y%m%d") -> str:
     date = datetime.date.fromisoformat(iso_date)
     return date.strftime(custom_format)
 
@@ -18,4 +18,4 @@ def updated_ids() -> bool:
     """
     return iso_date2custom_format(
         str(datetime.date.today()), custom_format=config["FORMAT_DATE"]
-    ) == str(config["LASTMOD_DATE"])
+    ) == str(config["LASTMOD"])
