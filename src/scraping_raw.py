ab229c69aabc33cdbc96c106e560212b0b48556e
Miguel Claramunt <claramuntmiguel@gmail.com>
2024-10-11 11:07:22 +0200

    ENH: mod tqdm total counter to display valid requests processed

--- src/scraping_raw.py
+++ src/scraping_raw.py
@@ -1,10 +1,9 @@
+import concurrent
 import json
-import os
 
 # import libsql_experimental as libsql
 import sqlite3
 
-import concurrent
 from tqdm import tqdm
 
 from mercatracker import api, db, logging
@@ -15,20 +14,26 @@ BATCH_SIZE = 50
 
 config = Config().load()
 
+
 def fetch_product_data(id, params):
     product = api.ProductSchema(id=id, params=params)
     product.request()  # Perform the request
     return product  # Return the ProductSchema instance
 
+
 def process_batch(ids_batch):
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(fetch_product_data, id, config.params_api) for id in ids_batch]
-        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+        futures = [
+            executor.submit(fetch_product_data, id, config.params_api)
+            for id in ids_batch
+        ]
+        results = [
+            future.result() for future in concurrent.futures.as_completed(futures)
+        ]
     return results
 
-def main():
-    config = Config().load()
 
+def main():
     conn = sqlite3.connect("/home/miguel/git/mercadona-tracker/src/mercadona.db")
 
     soup = Soup(url=config.url_sitemap).request()
@@ -46,7 +51,6 @@ def main():
 
     else:
         all_ids = db.get_scraped_ids(conn, lastmod)
-
 
     ids = set(all_ids) - set(db.get_processed_ids(conn, lastmod))
     logging.soup(lastmod)
@@ -83,23 +87,40 @@ def main():
     #             sys.exit(130)
     #         except SystemExit:
     #             os._exit(130)
-    
+
     ### MULTI THREAD PROCESSING ###
 
     ids_ = list(ids)
+    invalid_requests = []
 
     with tqdm(total=len(ids_)) as pbar:
         for i in range(0, len(ids_), BATCH_SIZE):
             batch_ids = ids_[i:i + BATCH_SIZE]
             results = process_batch(batch_ids)
-            
+
+            # update tqdm's total counter
+            invalid_requests.append(
+                [result for result in results if result.response.status_code != 200]
+            )
+            pbar.total = len(ids) - len(invalid_requests[0])
+            pbar.refresh()
+
             # write only valid requests to db
-            valid_requests = [result for result in results if result.response.status_code == 200]
-        
+            valid_requests = [
+                result for result in results if result.response.status_code == 200
+            ]
+
             for product in valid_requests:
-                db.write_dump(conn, (product.id, str(product.decode()), lastmod,),)
+                db.write_dump(
+                    conn,
+                    (
+                        product.id,
+                        str(product.decode()),
+                        lastmod,
+                    ),
+                )
                 pbar.update(1)
-    
+
 
 if __name__ == "__main__":
     main()
