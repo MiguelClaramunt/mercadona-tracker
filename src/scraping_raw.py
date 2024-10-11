7ec7d39d75d46fc8b23a56df0d0f995619f77be1
Miguel Claramunt <claramuntmiguel@gmail.com>
2024-10-11 10:30:05 +0200

    ENH: added multithreading

--- src/scraping_raw.py
+++ src/scraping_raw.py
@@ -3,74 +3,101 @@ import os
 
 # import libsql_experimental as libsql
 import sqlite3
-import sys
 
-import pandas as pd
+import concurrent
 from tqdm import tqdm
 
 from mercatracker import api, db, logging
 from mercatracker.config import Config
 from mercatracker.scraper import Soup
 
-pd.set_option("future.no_silent_downcasting", True)
+BATCH_SIZE = 50
 
 config = Config().load()
 
-conn = sqlite3.connect("/home/miguel/git/mercadona-tracker/src/mercadona.db")
-cur = conn.cursor()
+def fetch_product_data(id, params):
+    product = api.ProductSchema(id=id, params=params)
+    product.request()  # Perform the request
+    return product  # Return the ProductSchema instance
 
-soup = Soup(url=config.url_sitemap).request()
-lastmod = soup.get_lastmod()
+def process_batch(ids_batch):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(fetch_product_data, id, config.params_api) for id in ids_batch]
+        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+    return results
 
-if lastmod != db.get_lastmod(conn):
-    all_ids = soup.get_ids()
-    db.write_scraped_ids(
-        conn,
-        (
-            lastmod,
-            json.dumps(all_ids),
-        ),
-    )
-    # config["LASTMOD"] = lastmod
-    # config.update(file="temp", var={"LASTMOD": lastmod})
-else:
-    all_ids = db.get_scraped_ids(conn, lastmod)
+def main():
+    config = Config().load()
+
+    conn = sqlite3.connect("/home/miguel/git/mercadona-tracker/src/mercadona.db")
+
+    soup = Soup(url=config.url_sitemap).request()
+    lastmod = soup.get_lastmod()
+
+    if lastmod != db.get_lastmod(conn):
+        all_ids = soup.get_ids()
+        db.write_scraped_ids(
+            conn,
+            (
+                lastmod,
+                json.dumps(all_ids),
+            ),
+        )
+
+    else:
+        all_ids = db.get_scraped_ids(conn, lastmod)
 
 
-ids = set(all_ids) - set(db.get_processed_ids(conn, lastmod))
-logging.soup(lastmod)
+    ids = set(all_ids) - set(db.get_processed_ids(conn, lastmod))
+    logging.soup(lastmod)
 
 
-with tqdm(total=len(ids)) as pbar:
-    try:
-        for id in ids:
-            product = api.ProductSchema(id=id, params=config.params_api)
-            # request = api.ProxyScrapeProductRequest()
-            # response = request.post(product.url)
-            response = product.request_force_retry()
+    # with tqdm(total=len(ids)) as pbar:
+    #     try:
+    #         for id in ids:
+    #             try:
+    #                 product = api.ProductSchema(id=id, params=config.params_api)
+    #                 response = product.request()
 
-            if response.status_code == 200:
-                db.write_dump(
-                    conn,
-                    (
-                        id,
-                        # str(request.decode()),
-                        str(product.decode()),
-                        lastmod,
-                    ),
-                )
+    #                 if response.status_code == 200:
+    #                     db.write_dump(
+    #                         conn,
+    #                         (
+    #                             id,
+    #                             str(product.decode()),
+    #                             lastmod,
+    #                         ),
+    #                     )
+    #                     pbar.update(1)
+
+    #                 elif response.status_code == 410:
+    #                     continue
+
+    #             except requests.exceptions.SSLError:
+    #                 continue
+
+    #     except KeyboardInterrupt:
+    #         conn.close()
+    #         try:
+    #             sys.exit(130)
+    #         except SystemExit:
+    #             os._exit(130)
+    
+    # Convert set to list
+    ids_ = list(ids)
+
+    # Process in batches of BATCH_SIZE
+    with tqdm(total=len(ids_)) as pbar:
+        for i in range(0, len(ids_), BATCH_SIZE):
+            batch_ids = ids_[i:i + BATCH_SIZE]
+            results = process_batch(batch_ids)
+            
+            valid_requests = [result for result in results if result.response.status_code == 200]
+        
+            for product in valid_requests:
+                db.write_dump(conn, (product.id, str(product.decode()), lastmod,),)
                 pbar.update(1)
-                # ids.remove(id)
+    
 
-            if response.status_code == 410:
-                continue
-                # print(all_ids.index(id))
-                # ids.remove(id)
-                # config.update(file="temp", var={"ALL_IDS": all_ids})
-
-    except KeyboardInterrupt:
-        conn.close()
-        try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
+if __name__ == "__main__":
+    main()
