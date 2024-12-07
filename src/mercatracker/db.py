8bb2e598ae8f42d98ed48ab646f325bc883c8d37
Miguel Claramunt <claramuntmiguel@gmail.com>
2024-12-07 23:00:50 +0100

    Refactor database table creation and internal logic

--- src/mercatracker/db.py
+++ src/mercatracker/db.py
@@ -19,14 +19,11 @@ def dict_to_query(input_dict: dict[str, Any]) -> tuple[str, str]:
 def create_dumps_table(conn: sqlite3.Connection):
     conn.execute("""
                  CREATE TABLE IF NOT EXISTS "dumps" (
-                    "id" TEXT,
+                    "id" TEXT, 
                     "ymd" INTEGER,
                     "content" TEXT,
-                    "hash" INTEGER, 
-                    `ymd_id` INTEGER REFERENCES `dates_scraped`(`ymd_id`),
-                    PRIMARY KEY (id, ymd_id))
-                 ;""")
-    
+                    PRIMARY KEY (id, ymd));""")
+
     # Check if the trigger already exists
     cursor = conn.cursor()
     cursor.execute("""
@@ -37,44 +34,44 @@ def create_dumps_table(conn: sqlite3.Connection):
 
     if not trigger_exists:
         # Create a trigger to remove consecutive duplicates
-        # Duplicates are considered consecutive if they have same {id, content} than the previous ymd_id
+        # Duplicates are considered consecutive if they have same {id, content} than the previous ymd
         conn.execute("""
                     CREATE TRIGGER remove_consecutive_duplicates
                     AFTER INSERT ON dumps
                     BEGIN
-                        DELETE FROM dumps
-                        WHERE rowid = NEW.rowid AND EXISTS (
-                            SELECT 1 FROM dumps
-                            WHERE id = NEW.id
-                            AND ymd_id = (
-                                SELECT MAX(ymd_id) FROM dumps
-                                WHERE id = NEW.id AND ymd_id < NEW.ymd_id
-                            )
-                            AND content = NEW.content
-                        );
+                    DELETE FROM dumps
+                    WHERE rowid = NEW.rowid AND EXISTS (
+                        SELECT 1 FROM dumps
+                        WHERE id = NEW.id
+                        AND ymd = (
+                            SELECT MAX(ymd) FROM dumps
+                            WHERE id = NEW.id AND ymd < NEW.ymd
+                        )
+                        AND content = NEW.content
+                    );
                     END;
                     """)
     conn.commit()
 
 
-def create_ids_scraped_table(conn: sqlite3.Connection):
-    conn.execute("""
-                 CREATE TABLE IF NOT EXISTS ids_scraped (
-                    ymd TEXT,
-                    ids TEXT,
-                    PRIMARY KEY (ymd)
-                 );""")
-    conn.commit()
+# def create_ids_scraped_table(conn: sqlite3.Connection):
+#     conn.execute("""
+#                  CREATE TABLE IF NOT EXISTS ids_scraped (
+#                     ymd TEXT,
+#                     ids TEXT,
+#                     PRIMARY KEY (ymd)
+#                  );""")
+#     conn.commit()
 
 
-def create_dates_scraped_table(conn: sqlite3.Connection):
-    conn.execute("""
-                 CREATE TABLE IF NOT EXISTS dates_scraped (
-                    ymd INTEGER,
-                    ymd_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    UNIQUE (ymd, ymd_id)
-                 );""")
-    conn.commit()
+# def create_dates_scraped_table(conn: sqlite3.Connection):
+#     conn.execute("""
+#                  CREATE TABLE IF NOT EXISTS dates_scraped (
+#                     ymd INTEGER,
+#                     ymd_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                     UNIQUE (ymd, ymd_id)
+#                  );""")
+#     conn.commit()
 
 
 def write_dump(conn: sqlite3.Connection, parameters: dict):
@@ -85,36 +82,36 @@ def write_dump(conn: sqlite3.Connection, parameters: dict):
     conn.commit()
 
 
-def write_lastmod(conn: sqlite3.Connection, lastmod: int):
-    create_dates_scraped_table(conn)
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO dates_scraped(ymd) VALUES(?)""", (lastmod,))
-    conn.commit()
+# def write_lastmod(conn: sqlite3.Connection, lastmod: int):
+#     create_dates_scraped_table(conn)
+#     cur = conn.cursor()
+#     cur.execute("""INSERT INTO dates_scraped(ymd) VALUES(?)""", (lastmod,))
+#     conn.commit()
 
 
-def write_scraped_ids(conn: sqlite3.Connection, parameters: tuple):
-    create_ids_scraped_table(conn)
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO ids_scraped(ymd, ids) VALUES(?,?)""", parameters)
-    conn.commit()
+# def write_scraped_ids(conn: sqlite3.Connection, parameters: tuple):
+#     create_ids_scraped_table(conn)
+#     cur = conn.cursor()
+#     cur.execute("""INSERT INTO ids_scraped(ymd, ids) VALUES(?,?)""", parameters)
+#     conn.commit()
 
 
-def get_lastmod(conn: sqlite3.Connection) -> tuple[int, int]:
-    create_dates_scraped_table(conn)
-    return conn.execute("""SELECT max(ymd), ymd_id FROM dates_scraped""").fetchone()
+def get_lastmod(conn: sqlite3.Connection) -> int:
+    conn.row_factory = lambda cursor, row: row[0]
+    return conn.execute("""SELECT max(ymd) FROM dumps""").fetchone()
 
 
-def get_processed_ids(conn: sqlite3.Connection, ymd_id: str | int) -> list[str]:
+def get_processed_ids(conn: sqlite3.Connection, ymd: str | int) -> list[str]:
     create_dumps_table(conn)
     conn.row_factory = lambda cursor, row: row[0]
-    return conn.execute(
-        """SELECT id FROM dumps WHERE ymd_id=?""", (int(ymd_id),)
-    ).fetchall()
+    return conn.execute("""SELECT id FROM dumps WHERE ymd=?""", (int(ymd),)).fetchall()
 
 
 def get_scraped_ids(conn, ymd):
     create_dumps_table(conn)
-    result = conn.execute("SELECT ids FROM ids_scraped WHERE ymd = ?", (ymd,)).fetchone()
+    result = conn.execute(
+        "SELECT ids FROM ids_scraped WHERE ymd = ?", (ymd,)
+    ).fetchone()
     if result:
         return json.loads(result)  # Extract the string from the tuple
     return []
