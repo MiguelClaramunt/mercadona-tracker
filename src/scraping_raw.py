40386dee5da81aeb9ed2f71eb461f8d8fee62101
Miguel Claramunt <claramuntmiguel@gmail.com>
2025-02-03 21:04:34 +0100

    Refactor scrape_supermarket function to improve variable naming and streamline data handling

--- src/scraping_raw.py
+++ src/scraping_raw.py
@@ -18,10 +18,11 @@ from mercatracker.sethandler import SetHandler
 BATCH_SIZE = 10
 config = Config().load()
 
+
 def fetch_product_data(request_handler, id_, params):
     product = request_handler(id=id_, params=params)
-    product.request()
-    return product
+    return product.request()
+
 
 def process_batch(request_handler, ids_batch, params):
     with ThreadPoolExecutor(max_workers=4) as executor:
@@ -31,36 +32,43 @@ def process_batch(request_handler, ids_batch, params):
         ]
         return [f.result() for f in as_completed(futures)]
 
+
 def scrape_supermarket(conn, request_handler_class, sh, supermarket_params):
-    soup = request_handler_class(request_sitemap=True)
-    supermarket_id = db.query_database(conn, supermarket_params)
+    supermarket = request_handler_class(request_sitemap=True)
+    db_params = {
+        "supermarket": request_handler_class.get_name(),
+        "params": supermarket_params,
+    }
+    supermarket_id = db.query_database(conn, db_params)
     if not supermarket_id:
         db.write_supermarket_params(conn, supermarket_params)
         supermarket_id = db.query_database(conn, supermarket_params)
 
     sh.supermarket_id = supermarket_id
-    sh.ymd = soup.lastmod
+    sh.ymd = supermarket.lastmod
     sh.load_cache()
 
-    if soup.lastmod != db.get_lastmod(conn, supermarket_id):
-        logging.soup(soup.lastmod)
-        scraped_ids = soup.scrape_ids()
-        sh.reset_set("checked_ids", "written_ids")
-        sh.update_set("scraped_ids", scraped_ids, reset=True)
+    if supermarket.lastmod != db.get_lastmod(conn, supermarket_id):
+        logging.soup(supermarket.lastmod)
+        s = supermarket.scrape_ids()
+        sh.reset_set("c", "w")
+        sh.update_set("s", s, reset=True)
         sh.save_cache()
     else:
-        written_ids = db.get_processed_ids(conn, supermarket_id, soup.lastmod)
-        sh.update_set("written_ids", written_ids, reset=True)
+        w = db.get_processed_ids(conn, supermarket_id, supermarket.lastmod)
+        sh.update_set("w", w, reset=True)
 
     # Process IDs
-    ids_to_process = list(sh.scraped_ids - sh.written_ids - sh.checked_ids)
+    ids_to_process = list(sh.s - sh.w - sh.c)
     if not ids_to_process:
         return sh
 
     with tqdm(total=len(ids_to_process)) as pbar:
         for i in range(0, len(ids_to_process), BATCH_SIZE):
             batch_ids = ids_to_process[i : i + BATCH_SIZE]
-            results = process_batch(request_handler_class, batch_ids, supermarket_params)
+            results = process_batch(
+                request_handler_class, batch_ids, supermarket_params
+            )
             valid_requests = [r for r in results if r.response.status_code == 200]
             written_ids_batch = []
             for product in valid_requests:
@@ -69,7 +77,7 @@ def scrape_supermarket(conn, request_handler_class, sh, supermarket_params):
                         conn,
                         {
                             "id": product.id,
-                            "ymd": soup.lastmod,
+                            "ymd": supermarket.lastmod,
                             "content": product.to_dump(),
                             "supermarket_id": supermarket_id,
                         },
@@ -81,44 +89,41 @@ def scrape_supermarket(conn, request_handler_class, sh, supermarket_params):
 
             sh.update_set(
                 {
-                    "written_ids": written_ids_batch,
-                    "checked_ids": batch_ids,
+                    "w": written_ids_batch,
+                    "c": batch_ids,
                 }
             )
     return sh
 
+
 if __name__ == "__main__":
     conn = sqlite3.connect("/home/miguel/git/mercadona-tracker/src/mercadona.db")
     sh = SetHandler(conn=conn)
-    sh.init_set("scraped_ids", "written_ids", "checked_ids")
+    sh.init_set("s", "w", "c")  # scraped, written, checked
 
     scrape_targets = [
         {
             "request_handler": MercadonaProductSchema,
-            "params": {
-                "supermarket": "mercadona",
-                "params": '{"lang":"es","wh":"vlc1"}',
-            },
+            "params": '{"lang":"es","wh":"vlc1"}',
         },
         {
             "request_handler": ConsumProductSchema,
-            "params": {
-                "supermarket": "consum",
-                "params": '{"shippingZoneId":"0D"}',
-            },
+            "params": '{"shippingZoneId":"0D"}',
         },
     ]
 
     for target in scrape_targets:
-        sh.reset_set("scraped_ids", "written_ids", "checked_ids")
+        sh.reset_set("s", "w", "c")
 
         while True:
             try:
-                sh = scrape_supermarket(conn, target["request_handler"], sh, target["params"])
-                remaining_ids = sh.scraped_ids - sh.written_ids - sh.checked_ids
+                sh = scrape_supermarket(
+                    conn, target["request_handler"], sh, target["params"]
+                )
+                remaining_ids = sh.s - sh.w - sh.c
                 if not remaining_ids:
                     sh.save_cache()
-                    print(f"All {target['params']['supermarket']} IDs processed.")
+                    print(f"All {target['request_handler'].get_name()} IDs processed.")
                     break
             except requests.exceptions.SSLError:
                 secs = 120
